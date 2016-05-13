087600b33d066883f7e1b0ceee66857b7b25d17f
tinini <tinini.89@gmail.com>
2016-05-13 00:08:55 -0300

    With the rake task for seeding the db with the csv file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+require 'csv'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +6,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+      csv_text = File.open('geo_sampa.csv',"r:ISO-8859-1")
+      csv = CSV.parse(csv_text, :headers => true)
+      csv.each do |row|
+      puts row
+      Facility.create!(row.to_hash)
+      puts "Import done"
+  end
